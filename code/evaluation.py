38f7f59c83322a0c3454f595eacebec66ed257c2
ReemAlenezi <r.alenezi@grad.ku.edu.kw>
2026-03-28 17:24:01 +0300

    Update evaluation.py

--- code/evaluation.py
+++ code/evaluation.py
@@ -1,6 +1,6 @@
 from bert_score import score as bert_score
 
-def calculate_bertscore(references, candidates, lang="en"):
+def calculate_bertscore(references, candidates, lang="Ar"):
     P, R, F1 = bert_score(
         candidates, references,
         lang=lang,
